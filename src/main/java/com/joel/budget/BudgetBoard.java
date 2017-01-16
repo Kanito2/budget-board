47fd35d6e4aa474df58ff476f91ce7752ac265e9
Joel Andersson <anderssonjoel1@gmail.com>
2017-01-16 17:39:08 +0100

    Add basic GUI and images

--- src/main/java/com/joel/budget/BudgetBoard.java
+++ src/main/java/com/joel/budget/BudgetBoard.java
@@ -3,18 +3,42 @@
  */
 package com.joel.budget;
 
+import java.awt.*;
+import javax.swing.*;
+
 /**
  * @author Joel
  *
  */
-public class BudgetBoard {
 
+/* BudgetBoard.java requires no other files. */
+public class BudgetBoard {
 	/**
-	 * @param args
+	 * Create the GUI and show it.  For thread safety,
+	 * this method should be invoked from the
+	 * event-dispatching thread.
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hej");
+	private static void createAndShowGUI() {
+		//Create and set up the window.
+		JFrame frame = new JFrame("BudgetBoard");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JLabel emptyLabel = new JLabel("");
+		emptyLabel.setPreferredSize(new Dimension(300, 200));
+		frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+
+		//Display the window.
+		frame.pack();
+		frame.setVisible(true);
 	}
 
+	public static void main(String[] args) {
+		//Schedule a job for the event-dispatching thread:
+		//creating and showing this application's GUI.
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});
+	}
 }
