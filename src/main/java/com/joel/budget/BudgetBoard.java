adac49a0dcd9f72c5ba2dc18ad2c2bd3985b4aac
Joel Andersson <anderssonjoel1@gmail.com>
2017-01-16 17:04:33 +0100

    First commit

---
+++ src/main/java/com/joel/budget/BudgetBoard.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.joel.budget;
+
+/**
+ * @author Joel
+ *
+ */
+public class BudgetBoard {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hej");
+	}
+
+}
