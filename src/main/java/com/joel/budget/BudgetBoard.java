4cba9cfb20722a2e575416a8d95b01f0bf387e9c
Joel Andersson <anderssonjoel1@gmail.com>
2017-01-19 18:33:16 +0100

    Add file drag and drop

--- src/main/java/com/joel/budget/BudgetBoard.java
+++ src/main/java/com/joel/budget/BudgetBoard.java
@@ -4,6 +4,15 @@
 package com.joel.budget;
 
 import java.awt.BorderLayout;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DropTarget;
+import java.awt.dnd.DropTargetDragEvent;
+import java.awt.dnd.DropTargetDropEvent;
+import java.awt.dnd.DropTargetEvent;
+import java.awt.dnd.DropTargetListener;
+import java.io.File;
+import java.io.FileReader;
 
 import javax.swing.*;
 
@@ -17,6 +26,9 @@ public class BudgetBoard {
 	 * Create the GUI and show it. For thread safety, this method should be
 	 * invoked from the event-dispatching thread.
 	 */
+
+	private static JTextArea jt;
+
 	private static void createAndShowGUI() {
 		// Create and set up the window.
 		JFrame frame = new JFrame("BudgetBoard");
@@ -27,13 +39,53 @@ public class BudgetBoard {
 		JLabel label = new JLabel("Hello World");
 		frame.getContentPane().add(label, BorderLayout.NORTH);
 
-		int[] data = { 8, 2, 1, 0, 4, 9 };
-		ImagePanel ip = new ImagePanel(data);
-		frame.getContentPane().add(ip, BorderLayout.CENTER);
+//		int[] data = { 8, 2, 1, 0, 4, 9 };
+//		ImagePanel ip = new ImagePanel(data);
+//		frame.getContentPane().add(ip, BorderLayout.CENTER);
+
+		jt = new JTextArea();
+		frame.add(jt, BorderLayout.CENTER);
+
+		enableDragAndDrop();
 
 		// Display the window.
 		// frame.pack();
 		frame.setVisible(true);
+	}
+
+	private static void enableDragAndDrop() {
+		DropTarget target = new DropTarget(jt, new DropTargetListener() {
+			public void dragEnter(DropTargetDragEvent e) {
+			}
+
+			public void dragExit(DropTargetEvent e) {
+			}
+
+			public void dragOver(DropTargetDragEvent e) {
+			}
+
+			public void dropActionChanged(DropTargetDragEvent e) {
+
+			}
+
+			public void drop(DropTargetDropEvent e) {
+				try {
+					// Accept the drop first, important!
+					e.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+
+					// Get the files that are dropped as java.util.List
+					java.util.List list = (java.util.List) e.getTransferable()
+							.getTransferData(DataFlavor.javaFileListFlavor);
+
+					// Now get the first file from the list,
+					File file = (File) list.get(0);
+					System.out.println(file.getAbsolutePath());
+//					jt.read(new FileReader(file), null);
+
+				} catch (Exception ex) {
+				}
+			}
+		});
 	}
 
 	public static void main(String[] args) {
