ebb74e069cd61c73c7f3662f7cf4c34a734892c1
Joel Andersson <anderssonjoel1@gmail.com>
2017-01-19 17:39:48 +0100

    Replaced GUI

--- src/main/java/com/joel/budget/BudgetBoard.java
+++ src/main/java/com/joel/budget/BudgetBoard.java
@@ -3,7 +3,6 @@
  */
 package com.joel.budget;
 
-import java.awt.*;
 import javax.swing.*;
 
 /**
@@ -12,23 +11,27 @@ import javax.swing.*;
  */
 
 public class BudgetBoard {
-
+	/**
+	 * Create the GUI and show it. For thread safety, this method should be
+	 * invoked from the event-dispatching thread.
+	 */
 	private static void createAndShowGUI() {
+		// Create and set up the window.
 		JFrame frame = new JFrame("BudgetBoard");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setPreferredSize(new Dimension(600, 400));
 
-		JLabel label = new JLabel("Test");
-		frame.getContentPane().add(label, BorderLayout.NORTH);
+		// Add the ubiquitous "Hello World" label.
+		JLabel label = new JLabel("Hello World");
+		frame.getContentPane().add(label);
 
-		ImagePanel ip = new ImagePanel();
-		frame.getContentPane().add(ip, BorderLayout.CENTER);
-
+		// Display the window.
 		frame.pack();
 		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
+		// Schedule a job for the event-dispatching thread:
+		// creating and showing this application's GUI.
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
